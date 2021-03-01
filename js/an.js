8e7eede6c78271ff12b4fe16074f5335b6fa5395
Pacs <frjamezc@ucsc.edu>
2021-02-28 21:40:32 -0800

    all pics

--- js/an.js
+++ js/an.js
@@ -1,27 +1,14 @@
-// we will add this content, replace for anything you want to add
-var more = '<div style="height: 1000px; background: #EEE;"></div>';
+$(document).ready(function() {
+  var win = $(window);
 
-var wrapper = document.getElementById("wrapper");
-var content = document.getElementById("content");
-var test = document.getElementById("test");
-content.innerHTML = more;
+  // Each time the user scrolls
+  win.scroll(function() {
+    // End of the document reached?
+    if ($(document).height() - win.height() == win.scrollTop()) {
 
-// cross browser addEvent, today you can safely use just addEventListener
-function addEvent(obj,ev,fn) {
-  if(obj.addEventListener) obj.addEventListener(ev,fn,false);
-  else if(obj.attachEvent) obj.attachEvent("on"+ev,fn);
-}
-
-// this is the scroll event handler
-function scroller() {
-  // print relevant scroll info
-  test.innerHTML = wrapper.scrollTop+"+"+wrapper.offsetHeight+"+100>"+content.offsetHeight;
-
-  // add more contents if user scrolled down enough
-  if(wrapper.scrollTop+wrapper.offsetHeight+100>content.offsetHeight) {
-    content.innerHTML+= more;
-  }
-}
-
-// hook the scroll handler to scroll event
-addEvent(wrapper,"scroll",scroller);
+      $('html, body').animate({
+        scrollTop: $(".row").offset().top
+      },400);
+    }
+  });
+});
